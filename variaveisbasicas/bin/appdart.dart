62419963e9e6c26b59a266cb1c37bd7393701509
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2022-03-02 11:09:53 -0300

    colocando arquivos

---
+++ variaveisbasicas/bin/appdart.dart
@@ -0,0 +1,10 @@
+void main() {
+  var nomes = ['Sergio', 'Jeuendel', 'Carlos', 'Rafael', 'Julio'];
+  var quantElemento = nomes.length;
+  print(quantElemento);
+  var contador = 0;
+  while (contador < quantElemento) {
+    print(nomes[contador]);
+    contador++;
+  }
+}
