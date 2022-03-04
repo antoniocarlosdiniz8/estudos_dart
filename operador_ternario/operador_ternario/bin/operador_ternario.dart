cb3c56747ccb6ac34f0b89ba92ae2eb7c284d1c0
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2022-03-04 06:52:48 -0300

    criei operador ternario em dart

---
+++ operador_ternario/operador_ternario/bin/operador_ternario.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  //Variável
+  double nota = 7;
+  //Variável que recebe o resultado da operação
+  String nome = nota >= 6 ? 'Aprovado' : 'Reprovado';//Operador ternário na pratica
+  print(nome);//Saída do resultado
+}
