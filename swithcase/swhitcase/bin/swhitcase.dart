2a9f959d8f968589d411284397018a0ea338ca64
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2022-03-06 06:59:21 -0300

    Criei o exercicio switch case

---
+++ swithcase/swhitcase/bin/swhitcase.dart
@@ -0,0 +1,21 @@
+void main() {
+  String linguagem = 'Python';
+
+  switch (linguagem) {
+    case 'Dart':
+      print('Melhor Linguagem!');
+      break;
+    case 'Java':
+      print('Linguagem Obsoleta!');
+      break;
+    case 'Python':
+      print('Linguagem mais fácil do mundo!');
+      break;
+    case 'C':
+      print('Linguagem Compricada!');
+      break;
+    default:
+      print('Linguagem Desconhecida!');
+  }
+  
+}
