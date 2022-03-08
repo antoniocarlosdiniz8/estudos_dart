62bbebeb8b41cf79991713e3b165d64b6e68163a
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2022-03-08 11:25:23 -0300

    exemplo de string raw

---
+++ rawstring/rawstring/bin/rawstring.dart
@@ -0,0 +1,8 @@
+void main() {
+  var stringX = 'O valor ra rescissão é R\$ 10,000';
+  print(stringX);
+  //Indicando pro dart que não tem nenhum tipo de processamento
+  var stringY = r'O valor ra rescissão é R$ 15,000';
+  print(stringY);
+  
+}
